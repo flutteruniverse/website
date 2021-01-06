4a77f0a584cda4abd45cb33398d09b3a866262d3
Matias de Andrea <deandreamatias@gmail.com>
2021-01-06 19:13:16 +0100

    feat: Create base responsive

--- lib/core/pages/home_page.dart
+++ lib/core/pages/home_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boxicons/flutter_boxicons.dart';
+import 'package:sizer/sizer.dart';
 
 import '../router/router.dart';
 
@@ -15,45 +17,44 @@ class SideMenuItem {
   });
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  Widget build(_) => AutoRouter(
+        builder: (context, child) {
+          return Material(
+            child: SizerUtil.deviceType == DeviceType.Mobile
+                ? SmallLayout(
+                    child: child,
+                    router: context.router,
+                  )
+                : LargeLayout(
+                    child: child,
+                    router: context.router,
+                  ),
+          );
+        },
+      );
 }
 
-class _HomePageState extends State<HomePage> {
+class LargeLayout extends StatelessWidget {
+  const LargeLayout({Key key, this.child, this.router}) : super(key: key);
+  final Widget child;
+  final StackRouter router;
+
   final _sideMenuItems = const <SideMenuItem>[
     SideMenuItem(
       destination: SettingsRoute(),
-      iconData: Icons.settings,
+      iconData: Boxicons.bx_slider,
       label: 'Settings',
     )
   ];
 
   @override
-  Widget build(_) => AutoRouter(
-        builder: (context, child) {
-          return Material(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Row(
-                children: [
-                  buildSideMenu(context),
-                  Expanded(child: child),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-
-  Widget buildSideMenu(BuildContext context) {
-    final router = context.router;
-    return SizedBox(
-      width: 240,
-      child: Card(
-        margin: const EdgeInsets.fromLTRB(16, 8, 16, 24),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16),
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 240,
           child: Column(
             children: _sideMenuItems
                 .map((item) => ListTile(
@@ -66,7 +67,25 @@ class _HomePageState extends State<HomePage> {
                 .toList(),
           ),
         ),
-      ),
+        const VerticalDivider(thickness: 1, width: 1),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(16.0.sp),
+            child: child,
+          ),
+        ),
+      ],
     );
+  }
+}
+
+class SmallLayout extends StatelessWidget {
+  const SmallLayout({Key key, this.child, this.router}) : super(key: key);
+  final Widget child;
+  final StackRouter router;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
   }
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 import 'core/router/router.dart';
 
@@ -11,12 +12,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: true,
-      title: 'Universo Flutter | Debug',
-      routerDelegate: appRouter.delegate(),
-      routeInformationParser: appRouter.defaultRouteParser(),
-      builder: (context, child) => child,
+    return LayoutBuilder(
+      builder: (context, constraints) => OrientationBuilder(
+        builder: (context, orientation) {
+          SizerUtil().init(constraints, orientation);
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: true,
+            title: 'Universo Flutter | Debug',
+            routerDelegate: appRouter.delegate(),
+            routeInformationParser: appRouter.defaultRouteParser(),
+            builder: (context, child) => child,
+          );
+        },
+      ),
     );
   }
 }
