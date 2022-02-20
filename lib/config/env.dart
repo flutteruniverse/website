a2b30f39ce3368aea271ee84d83f518db1cd8809
Matias de Andrea <deandreamatias@gmail.com>
2022-02-20 11:44:06 +0100

    feat: Improve env config

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,1 +1,7 @@
+// env.dart
+import 'dart:io';
 
+const Map<String, String> environment = <String, String>{
+  'spotifyClientId': String.fromEnvironment('SPOTIFY_CLIENT_ID'),
+  'spotifyClientSecret': String.fromEnvironment('SPOTIFY_CLIENT_SECRET'),
+};
