b0a0dda17c43b30bacbff6dec43c3faa5242ae6d
Matias de Andrea <deandreamatias@gmail.com>
2022-02-20 11:43:37 +0100

    refactor: Remove graphx dependency

--- lib/core/pages/layout_page.dart
+++ lib/core/pages/layout_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:graphx/graphx.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../shared/utils.dart';
